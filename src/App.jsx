80cb5eee83e929c64c1d38eb5e53d86cacdc3bcd
FoeSeaL <gunjanrajak2021@gmail.com>
2025-03-27 15:42:07 +0545

    commit by gunjan

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import './styles/Home.css'
 import Footer from './components/Footer'
+import Trek from './pages/Trek'
+import Tour from './pages/Tour'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <>
-      <Router>
+    <Router>
+     <>
         
         <Navbar />
         <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path='/trek' element={<Trek />} />
+        <Route path='/tour' element={<Tour />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         </Routes>
-        <Footer />
 
-      </Router>
-    </>
+        <Footer />
+      </>
+   </Router>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,87 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      
-<section className="hero position-relative">
-  <nav className="navbar navbar-expand-lg ">
-    <div className="container fs-5">
-      <a className="navbar-brand" href="#">
-        <img src="https://nepalwanders.com/wp-content/themes/nepalwanders/img/logo1.png" className="logo" /></a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
-        <li>
-          <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>HOME</NavLink>
-        </li>
-        <li>
-          <NavLink to="/trek" className={({ isActive }) => isActive ? "active" : ""}>TREK</NavLink>
-        </li>
-        <li>
-          <NavLink to="/tour" className={({ isActive }) => isActive ? "active" : ""}>TOUR</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>ABOUT</NavLink>
-        </li>
-        <li>  
-          <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink>
-        </li>
-      </ul>
-      </div>
-    </div>
-  </nav>
-</section>
+    <section className="hero position-relative">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container fs-5">
+          <a className="navbar-brand" href="#">
+            <img
+              src="https://nepalwanders.com/wp-content/themes/nepalwanders/img/logo1.png"
+              className="logo"
+              alt="Nepal Wanders Logo"
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                >
+                  HOME
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/trek"
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                >
+                  TREK
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/tour"
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                >
+                  TOUR
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                >
+                  ABOUT
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                >
+                  CONTACT
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+};
 
-
-    </>
-  )
-}
-
-export default Navbar
+export default Navbar;
